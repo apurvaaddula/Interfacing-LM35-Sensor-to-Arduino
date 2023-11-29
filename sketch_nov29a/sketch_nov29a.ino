b7ce12a2d1774ea515b8e304012185afc735d31a
Apurva Reddy Addula <66956207+apurvaaddula@users.noreply.github.com>
2023-11-29 12:14:27 +0530

    Add files via upload

---
+++ sketch_nov29a/sketch_nov29a.ino
@@ -0,0 +1,36 @@
+int ledState = LOW; //Initiliazing a variable ledState to LOW
+
+//Using counter variables to be used for obtaining delay instead of using functions like delay, millis, etc.
+int counter1 = 0; 
+int counter2 = 0;
+
+void setup() {
+  pinMode(13, OUTPUT); //Initialising LED pin 13 as output
+}
+
+void loop() {
+  //Reading LM35 data from pin A0
+  int sensorValue = analogRead(A0); 
+  float temp = (sensorValue * 0.00488) * 100.0;
+  
+  //Blink LED according to given conditions.
+  if (temp < 30.0) {
+    counter1 = 250;
+  } else {
+    counter1 = 500;
+  }
+
+  //Logic for delay without using delay functions.
+  if (counter2 < counter1) {
+    counter2++;
+  } else {
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(13, ledState);
+    counter2 = 0;
+  }
+  
+}
